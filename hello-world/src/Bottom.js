f3aa1b9d4355e2fe557004c305ed48225020c138
uc5804 <73331669+uc5804@users.noreply.github.com>
2021-05-04 20:41:54 +0900

    210504 템플릿구성

---
+++ hello-world/src/Bottom.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bottom(){
+  return <h1>Bottom</h1>
+}
+
+export default Bottom;
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+import Menu from './Menu';
+import Content from './Content';
+import Bottom from './Bottom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Header />, document.getElementById("header"));
+ReactDOM.render(<Menu />, document.getElementById("menu"));
+ReactDOM.render(<Content />, document.getElementById("content"));
+ReactDOM.render(<Bottom />, document.getElementById("bottom"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
