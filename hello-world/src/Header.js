69bffeeec5c22bbf9edce9e54d856ee009854a11
uc5804 <uc5804@naver.com>
2021-05-11 21:30:52 +0900

    initialize

--- hello-world/src/Header.js
+++ hello-world/src/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./css/style.css";
+import "./css/Header.css";
 
 function Header(){
-  return <h1>Header</h1>
+  return <div class="container d-flex align-items-center flex-column">
+            <img class="masthead-avatar mb-5" src="assets/img/hanjul.jpg" alt="" />
+            <h1 class="masthead-heading text-uppercase mb-0">한줄코딩</h1>
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <p class="masthead-subheading font-weight-light mb-0">웹개발자 커뮤니티</p>
+          </div>
 }
 
 export default Header;
--- hello-world/src/Menu.js
+++ hello-world/src/Menu.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./css/style.css";
+import "./css/Menu.css";
 
 function Menu(){
   return <h1>Menu</h1>
