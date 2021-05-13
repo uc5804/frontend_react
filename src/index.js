a09554a1a29a6d8becd8cbf03066148cba95b38c
uc5804 <uc5804@naver.com>
2021-05-13 16:43:04 +0900

    directory change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-import Nav from './Nav';
-import Menu from './Menu';
-import Section1 from './Section1';
-import Section2 from './Section2';
-import Section3 from './Section3';
-import Bottom from './Bottom';
-import Copyright from './Copyright';
+import Header from '../src/js/Header';
+import Nav from '../src/js/Nav';
+import Menu from '../src/js/Menu';
+import Section1 from '../src/js/Section1';
+import Section2 from '../src/js/Section2';
+import Section3 from '../src/js/Section3';
+import Bottom from '../src/js/Bottom';
 
 
 ReactDOM.render(<Nav />, document.getElementById("mainNav"));
@@ -17,5 +16,4 @@ ReactDOM.render(<Section1 />, document.getElementById("section1"));
 ReactDOM.render(<Section2 />, document.getElementById("section2"));
 ReactDOM.render(<Section3 />, document.getElementById("section3"));
 ReactDOM.render(<Bottom />, document.getElementById("bottom"));
-ReactDOM.render(<Copyright />, document.getElementById("copyright"));
 
